9f487d1616ab08938bdf30a0745074fcee899a6d
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-10-23 11:07:16 +0100

    refactor get_removed_users_from_audit_log() (#9)

--- landing_page_app/main/services/github_service.py
+++ landing_page_app/main/services/github_service.py
@@ -7,6 +7,13 @@ from datetime import datetime, timedelta
 class GithubService:
     def __init__(self, org_token: str) -> None:
         self.github_client_core_api: Github = Github(org_token)
+        self.github_client_rest_api = requests.Session()
+        self.github_client_rest_api.headers.update(
+            {
+                "Accept": "application/vnd.github+json",
+                "Authorization": f"Bearer {org_token}",
+            }
+        )
 
     def get_user(self, user_name: str) -> NamedUser:
         return self.github_client_core_api.get_user(user_name.lower())
@@ -16,47 +23,30 @@ class GithubService:
             email=email_address
         )
 
-    # Needs an access token as PyGithub has no Audit Log functionality TODO: good way to do this
-    def return_users_removed_from_audit_log(organization: str, access_token: str) -> list:
+    def get_removed_users_from_audit_log(self, organisation_name: str) -> list:
         users = set()
 
         # Calculate the date three months ago
         three_months_ago = (datetime.now() - timedelta(days=90)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
-        # Set initial call
-        url = f"https://api.github.com/orgs/{organization}/audit-log?phrase=action:org.remove_member&created_at=>{three_months_ago}&per_page=100"
+        pagination = True
+        url = f"https://api.github.com/orgs/{organisation_name}/audit-log?phrase=action:org.remove_member&created_at=>{three_months_ago}&per_page=100"
 
-        while True:
-            try:
-                headers = {
-                    "Authorization": f"token {access_token}",
-                }
+        while pagination:
+            # Get log
+            response = self.github_client_rest_api.get(url)
+            response.raise_for_status()
+            audit_log_data = response.json()
 
-                # Get log
-                response = requests.get(url, headers=headers)
-                response.raise_for_status()
+            # Extract users
+            for event in audit_log_data:
+                user = event.get("user")
+                if user:
+                    users.add(user)
 
-                audit_log_data = response.json()
-
-                # Extract users
-                for event in audit_log_data:
-                    user = event.get("user")
-                    if user:
-                        # Add to set
-                        users.add(user)
-
-                # Pagination
-                if "Link" in response.headers:
-                    links = response.headers["Link"]
-                    if 'rel="next"' not in links:
-                        break
-                    url = response.links["next"]["url"]
-                else:
-                    break
-
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                break
-
-        # Convert to list
+            # Check for next page in Pagination
+            if response.links.get("next") is not None:
+                url = response.links["next"]["url"]
+            else:
+                pagination = False
         return list(users)
--- tests/services/test_github_service.py
+++ tests/services/test_github_service.py
@@ -1,17 +1,22 @@
 import unittest
-from unittest.mock import patch, call, Mock
-from github.NamedUser import NamedUser
+from requests import Response
+from unittest.mock import patch, call, MagicMock
+from datetime import datetime, timedelta
 from landing_page_app.main.services.github_service import GithubService
+from freezegun import freeze_time
 
 
 @patch("github.Github.__new__")
+@patch("requests.sessions.Session.__new__")
 class TestGithubServiceInit(unittest.TestCase):
-    def test_sets_up_class(self, mock_github_client_core_api):
+    def test_sets_up_class(self, mock_github_client_rest_api, mock_github_client_core_api):
         mock_github_client_core_api.return_value = "test_mock_github_client_core_api"
         github_service = GithubService("")
         self.assertEqual(
             "test_mock_github_client_core_api", github_service.github_client_core_api
         )
+        mock_github_client_rest_api.assert_has_calls([call().headers.update(
+            {"Accept": "application/vnd.github+json", "Authorization": "Bearer "})])
 
 
 @patch("github.Github.__new__")
@@ -33,6 +38,49 @@ class TestGithubServiceAddNewUserToOrg(unittest.TestCase):
         github_service.github_client_core_api.get_organization.assert_has_calls(
             [call("some-org"), call().invite_user(email="approved@email.com")]
         )
+
+
+@freeze_time("2023-02-01")
+@patch("github.Github.__new__", new=MagicMock)
+@patch("requests.sessions.Session.__new__")
+class TestGithubServiceGetRemovedUsersFromAuditLog(unittest.TestCase):
+    def test_calls_downstream_services(self, mock_github_client_rest_api):
+        github_service = GithubService("")
+        response = MagicMock(Response)
+        response.links = {}
+        mock_github_client_rest_api.get.return_value = response
+        github_service.github_client_rest_api = mock_github_client_rest_api
+        users = github_service.get_removed_users_from_audit_log("test-org")
+        self.assertEqual(len(users), 0)
+        three_months_ago = (datetime.now() - timedelta(days=90)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        mock_github_client_rest_api.assert_has_calls(
+            [
+                call.get(f"https://api.github.com/orgs/test-org/audit-log?phrase=action:org.remove_member&created_at=>{three_months_ago}&per_page=100"),
+                call.get().raise_for_status(),
+                call.get().json(),
+                call.get().json().__iter__()
+            ]
+        )
+
+    def test_get_removed_users_from_audit_log_return_users(self, mock_github_client_rest_api):
+        github_service = GithubService("")
+        response = MagicMock(Response)
+        response.links = {}
+        response.json.return_value = [{"user": "some-user"}]
+        mock_github_client_rest_api.get.return_value = response
+        github_service.github_client_rest_api = mock_github_client_rest_api
+        users = github_service.get_removed_users_from_audit_log("test-org")
+        self.assertEqual(len(users), 1)
+
+    def test_get_removed_users_from_audit_log_return_no_users(self, mock_github_client_rest_api):
+        github_service = GithubService("")
+        response = MagicMock(Response)
+        response.links = {}
+        response.json.return_value = []
+        mock_github_client_rest_api.get.return_value = response
+        github_service.github_client_rest_api = mock_github_client_rest_api
+        users = github_service.get_removed_users_from_audit_log("test-org")
+        self.assertEqual(len(users), 0)
 
 
 if __name__ == "__main__":
