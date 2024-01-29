7d8055c2dbb4cde3892b94d75a2b38889d6b8702
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-01-29 17:12:19 +0000

    :recycle: Change the GitHub Service tests to reflect refactor

--- tests/services/test_github_service.py
+++ tests/services/test_github_service.py
@@ -1,6 +1,8 @@
+import os
 import unittest
 from unittest.mock import call, patch
 
+from join_github_app.main.config.constants import MINISTRY_OF_JUSTICE
 from join_github_app.main.services.github_service import GithubService
 
 
@@ -25,6 +27,29 @@ class TestGithubServiceInit(unittest.TestCase):
                 )
             ]
         )
+
+
+@patch("github.Github.__new__")
+class TestGithubServiceInvites(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.valid_email = "test@test.com"
+        self.valid_orgs = ["test1", "test2"]
+
+    @patch.dict(os.environ, {"SEND_EMAIL_INVITES": "False"}, clear=True)
+    def test_send_email_invites_off(self, mock_github_client_rest_api):
+        github_service = GithubService("test")
+        github_service.github_client_rest_api = mock_github_client_rest_api
+
+        self.assertEqual(github_service.send_invitation_to_organisation(self.valid_email, self.valid_orgs), None)
+
+    @patch.dict(os.environ, {"SEND_EMAIL_INVITES": "True"}, clear=True)
+    def test_send_email_invites_on(self, mock_github_client_rest_api):
+        github_service = GithubService("test")
+        github_service.github_client_rest_api = mock_github_client_rest_api
+
+        github_service.send_invitation_to_organisation(self.valid_email, self.valid_orgs)
+        mock_github_client_rest_api.assert_called()
 
 
 if __name__ == "__main__":
