b5488ecd076dff5f2e47ebb6009fef8d5a125b9c
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2024-01-31 14:47:05 +0000

    🔥 Remove unused config (#128)

--- join_github_app/config/production.py
+++ join_github_app/config/production.py
@@ -1,16 +1,3 @@
-""" Config values to be used in production """
-from os import environ
-
-DEBUG = False
-FLASK_DEBUG = False
-MAIL_FROM_EMAIL = "operations-engineering@digital.justice.gov.uk"
-PORT = 4567
-SSL_REDIRECT = True
-TESTING = False
-AUTH0_CLIENT_ID = environ.get("AUTH0_CLIENT_ID")
-AUTH0_CLIENT_SECRET = environ.get("AUTH0_CLIENT_SECRET")
-AUTH0_DOMAIN = environ.get("AUTH0_DOMAIN")
-APP_SECRET_KEY = environ.get("APP_SECRET_KEY")
 SELECTABLE_ORGANISATIONS = [
     {'value': 'ministryofjustice', 'text': 'Ministry of Justice'},
     {'value': 'moj-analytical-services', 'text': 'MoJ Analytical Services'}
