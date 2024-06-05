6accf5221cece18af8d611d658260335dd77afb6
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-06-05 15:44:20 +0100

    :sparkles: Handle GithubException in send_invitation route
    This commit improves the `send_invitation` function in `join.py` by adding error handling for potential exceptions when trying to send GitHub invitations. Specifically, if a `GithubException` is encountered indicating that the user is already a member of the organization, it logs the event and returns an appropriate message. Other `GithubException` instances or `ValueError` will still cause the system to abort and log the error. Relevant tests have been added to ensure these exceptions are properly handled.
    
    Please note that this commit also includes minor changes to import statements and formatting which do not affect functionality.

--- tests/routes/test_join.py
+++ tests/routes/test_join.py
@@ -3,6 +3,7 @@ from types import SimpleNamespace
 from unittest.mock import MagicMock, patch
 
 from flask import get_flashed_messages
+from github import GithubException
 
 from app.app import create_app
 from app.main.services.github_service import GithubService
@@ -31,7 +32,8 @@ class TestSubmitEmail(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.request.path, "/join/submit-email")
-        self.assertEqual(flashed_message.get("message"), expected_flashed_message)
+        self.assertEqual(flashed_message.get(
+            "message"), expected_flashed_message)
 
     def test_redirects_to_select_organisation_when_email_is_pre_approved(self):
         form_data = {"emailAddress": "email@digital.justice.gov.uk"}
@@ -42,7 +44,8 @@ class TestSubmitEmail(unittest.TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.request.path, "/join/submit-email")
-        self.assertEqual(response.headers["Location"], "/join/select-organisations")
+        self.assertEqual(
+            response.headers["Location"], "/join/select-organisations")
         self.assertEqual(flashed_message.get("message"), None)
 
     def test_redirects_to_outside_collaborators_when_email_is_not_pre_approved(self):
@@ -54,7 +57,8 @@ class TestSubmitEmail(unittest.TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.request.path, "/join/submit-email")
-        self.assertEqual(response.headers["Location"], "/join/outside-collaborator")
+        self.assertEqual(
+            response.headers["Location"], "/join/outside-collaborator")
         self.assertEqual(flashed_message.get("message"), None)
 
 
@@ -130,6 +134,56 @@ class TestSelection(unittest.TestCase):
         self.assertIn("Office 365", str(response.data))
 
 
+class TestSendInvitations(unittest.TestCase):
+    def setUp(self):
+        self.github_service = MagicMock(GithubService)
+        self.app = create_app(self.github_service, False)
+        self.app.config["SECRET_KEY"] = "test_flask"
+        self.client = self.app.test_client()
+
+    @patch(
+        "app.main.routes.join.app_config",
+        new=SimpleNamespace(
+            github=SimpleNamespace(
+                organisations=[
+                    SimpleNamespace(
+                        name="ministryofjustice",
+                        enabled=True,
+                        display_text="Ministry of Justice",
+                    )
+                ]
+            )
+        ),
+    )
+    def test_when_user_is_already_member_of_an_org(self):
+
+        self.github_service.send_invites_to_user_email.side_effect = GithubException(
+            status=422,
+            data={
+                "message": "Validation Failed",
+                "errors": [
+                    {
+                        "resource": "OrganizationInvitation",
+                        "code": "unprocessable",
+                        "field": "data",
+                        "message": "A user with this email address is already a part of this organization"
+                    }
+                ],
+                "documentation_url": "https://docs.github.com/rest/orgs/members#create-an-organization-invitation"
+            }
+        )
+
+        with self.client.session_transaction() as sess:
+            sess["user"] = {"userinfo": {"email": "test@justice.gov.uk"}}
+            sess["user_input_email"] = "test@justice.gov.uk"
+            sess["org_selection"] = ["ministryofjustice"]
+
+        response = self.client.get("/join/send-invitation")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            "User test@justice.gov.uk is already a member of the organization.", response.data.decode())
+
+
 class TestInvitationSent(unittest.TestCase):
     def setUp(self):
         self.github_service = MagicMock(GithubService)
@@ -184,7 +238,8 @@ class TestInvitationSent(unittest.TestCase):
     def test_multiple_invitations_sent(self):
         with self.client.session_transaction() as sess:
             sess["user"] = {"userinfo": {"email": "test@justice.gov.uk"}}
-            sess["org_selection"] = ["ministryofjustice", "moj-analytical-services"]
+            sess["org_selection"] = [
+                "ministryofjustice", "moj-analytical-services"]
 
         response = self.client.get("/join/invitation-sent")
 
