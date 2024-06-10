126b38d5e5bec789c9d5daa40303649d2f388303
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2024-06-10 16:28:58 +0100

    🐛 Improve GitHub Exception Handling (#202)
    * :construction: Add pipeline to deploy the application to a test env
    
    This will allow me to test some changes to the application without making changes to the shared environments
    
    * :construction: Add debug logging and catch all errors
    
    * :construction: Add debugging messages to the failure status code
    
    We need to figure out what's being returned here.
    
    * :construction: Add the GitHubException handler as it's now confirmed
    
    * :recycle: Change wording of duplicate user message
    
    * :fire: Remove duplicate block from leftover code
    
    * :recycle: Make text slightly more readable
    
    * :recycle: Update CI/CD Deployment Configurations for Main Branch
    
    Reverts the previous debugging work.
    
    * :sparkles: Update README with instructions for enabling GitHub email invites
    
    * :fire: Remove unwanted code block
    
    * :art: Remove email display from already-a-member page

--- app/main/routes/join.py
+++ app/main/routes/join.py
@@ -132,7 +132,9 @@ def send_invitation():
         current_app.github_service.send_invites_to_user_email(
             auth0_email, org_selection)
     except GithubException as e:
-        if "A user with this email address is already a part of this organization" in str(e):
+        error_message = str(e)
+        logger.error("GithubException occurred: %s", error_message)
+        if "already a part of this organization" in error_message:
             logger.error(
                 "User %s is already a member of the organization.", auth0_email)
             return render_template(
@@ -140,7 +142,10 @@ def send_invitation():
                 email=auth0_email,
             )
         # re-raise the exception if it's a different error
-        logger.error("An unexpected GithubException occurred: %s", str(e))
+        logger.error("An unexpected GithubException occurred: %s", error_message)
+        raise e
+    except Exception as e:
+        logger.error("An unexpected exception occurred: %s", str(e))
         raise e
 
     return redirect(url_for("join_route.invitation_sent"))
